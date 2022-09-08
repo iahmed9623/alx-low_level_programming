98d356cb19a3f890824e13ba49cd6a0a0004a40c
iahmed9623 <ronldo024@gmail.com>
2022-09-08 16:20:00 -0700

    std

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studo.h>
+/**  
+*main - print the string in the put function
+*
+*Description: using rhe main function
+* this program prints ""Programming is like building a multilingual puzzle"
+*/
+int main (void)
+{
+           puts ("\"Programming is like building a multilingual puzzle");
+
+
+
+return (0);
+}
