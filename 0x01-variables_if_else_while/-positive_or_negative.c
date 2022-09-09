1ddf611272f147292cd88a66385c0e012ee57b89
iahmed9623 <ronldo024@gmail.com>
2022-09-09 15:44:34 -0700

    mmm

---
+++ 0x01-variables_if_else_while/-positive_or_negative.c
@@ -0,0 +1,23 @@
+
+
+
+
+#include <stdio.h>                                                                                                          
+
+/**                                                                                                                         
+
+ * main - Prints a text                                                                                                      
+
+ *                                                                                                                           
+
+ * Return: Always 0 (success)                                                                                                
+
+ */                                                                                                                          
+
+int main(void)                                                                                                              
+
+{                                                                                                                           
+
+printf("with proper grammar,but the outcome is a piece of art,\n");                                                         
+
+[A[A[B[B[B[B[B[B
