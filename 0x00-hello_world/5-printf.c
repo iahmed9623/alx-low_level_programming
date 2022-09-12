1970c854485db4133f92107a92225260d0c13533
iahmed9623 <ronldo024@gmail.com>
2022-09-12 15:05:53 -0700

    sdad

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,10 @@
-
 #include <stdio.h> 
 /**
- * main - Prints a text
- *
- * Return: Always 0 (success)
+ * Description: main - Prints out a string to stdout
+ * Return: 0 if succes
  */ 
 int main(void)
-{
-
-
-     
-
-    
-
-
-
-printf("with proper grammar,but the outcome is a piece of art,\n");
+{	
+printf("with proper grammar, but the outcome is a piece of art,\n");	
 return (0);
+}
