d37a78fd6144c4979e3e93ad3a179c89dbd108c7
iahmed9623 <ronldo024@gmail.com>
2022-09-12 18:39:23 -0700

    ASa

---
+++ 4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/**
+* main print the string in the put function
+* Description: using the main function
+* this program prints "Programing is like building a multilingual puzzle
+* Return: e
+int main(void)
