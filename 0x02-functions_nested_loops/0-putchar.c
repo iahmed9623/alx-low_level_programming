8d2e779959a9bdd254243902f7a7894da416e646
iahmed9623 <ronldo024@gmail.com>
2022-09-13 17:49:03 -0700

    sdasd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point 
- *
- * Return: Always sucess
- */
-int main ("_putchar\n");
+#include "main.h"
 
-return (0);
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0)
 }
