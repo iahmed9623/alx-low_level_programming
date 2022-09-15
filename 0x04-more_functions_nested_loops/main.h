955458a346c2ac3d2327bcca9f18b24774679984
iahmed9623 <ronldo024@gmail.com>
2022-09-15 16:45:50 -0700

    sadsd

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H_INCLUDED 
-#define MAIN_H_INCLUDED
-
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,6 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n)
+void print_number(int n);
 
 #endif /* MAIN_H */
