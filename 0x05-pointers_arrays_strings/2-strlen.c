d1b41cd7a2c94048063ecbd3194a2d3c33015540
iahmed9623 <ronldo024@gmail.com>
2022-09-19 16:52:20 -0700

    dasdas

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 
 /**
  *_strlen - returns the length of a string
- *@s: string
+ * @s: string
  *Return: returns length as integer;
  */
 
-int _strlen(char *s);
+int _strlen(char *s)
 {
-	int len = 0;
-		while (*(s + len) != '\0')
-					len++;
-			return (len);
 
+		int len = 0;
+			while (*(s + len) != '\0')
+						len++;
+				return (len);
 }
