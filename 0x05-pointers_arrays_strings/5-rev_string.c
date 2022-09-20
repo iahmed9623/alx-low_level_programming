5a23bb694f4e897cb772fa33510d622dabec97ee
iahmed9623 <ronldo024@gmail.com>
2022-09-19 18:35:03 -0700

    dasd

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,13 @@
 
 void rev_string(char *s)
 {
-		int i, tmp, len = _strlen(s);
+int i, tmp, len = _strlen(s);
 
-			for (i = 0; i < len / 2; i++)
-					{
-								tmp = *(s + i);
-										*(s + i) = *(s + len - i - 1);
-												*(s + len - i - 1) = tmp;
-													}
+for (i = 0; i < len / 2; i++)
+{
+tmp = *(s + i);
+*(s + i) = *(s + len - i - 1);
+*(s + len - i - 1) = tmp;													}
 }
 
 /**
@@ -30,8 +29,8 @@ void rev_string(char *s)
 int _strlen(char *s)
 
 {
-		int len = 0;
-			while (*(s + len) != '\0')
-						len++;
-				return (len);
+int len = 0;
+while (*(s + len) != '\0')
+len++;
+return (len);
 }
