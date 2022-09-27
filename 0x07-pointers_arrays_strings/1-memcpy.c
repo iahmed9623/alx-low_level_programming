0d1618cb50e679d6b4e2aa2c4023b12ab27d0084
iahmed9623 <ronldo024@gmail.com>
2022-09-26 19:00:06 -0700

    ads

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,12 +5,13 @@
   *
   *@dest: pointer that will receive the copied data
   *@src: pointer that has the information to be copied
-  *@n: amount of times the src information willbe copied to dest
+  *@n: amount of times the src information will be copied to dest
   *
   *Return: dest
   */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
 unsigned int i;
 
