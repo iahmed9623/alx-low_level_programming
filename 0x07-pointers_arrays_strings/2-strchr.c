7c7c4e848483ff503b7528dd349eef5c0f180fea
iahmed9623 <ronldo024@gmail.com>
2022-09-27 18:19:58 -0700

    sdsad

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,21 +2,22 @@
 
 /**
  * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to check for
  *
- * @s: string to search
- * @c: character to search for
- *
- * Return: pointer to first occurence of c in s
+ * Return: pointer to spot in s with c or null
  */
 
 char *_strchr(char *s, char c)
 {
-while (*s != 0)
+int i;
+
+for (i = 0; *(s + i); i++)
 {
-if (*s == c)
-return (s);
-s++;
-if (*s == c)
-return (s);
+if (*(s + i) == c)
+return (s + i);
+}
+if (*(s + i) == c)
+return (s + i);
 return (0);
 }
